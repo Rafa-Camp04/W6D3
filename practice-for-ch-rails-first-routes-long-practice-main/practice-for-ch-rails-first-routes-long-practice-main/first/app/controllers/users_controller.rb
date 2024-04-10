1a81c92f04711be818edbdec163cf659b3046fc0
katie leong <katieleong@yahoo.com>
2024-04-10 14:32:08 -0700

    finished phase 3

--- practice-for-ch-rails-first-routes-long-practice-main/practice-for-ch-rails-first-routes-long-practice-main/first/app/controllers/users_controller.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/practice-for-ch-rails-first-routes-long-practice-main/first/app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
 
     def index
-
         # @users = User.all
-
         render plain:"index_action"
-
     end
 
+    def create
+        render json: params
+    end
 
+    def show
+        render json: params
+    end
 end
